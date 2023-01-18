2267a1bc688941f5fcae0c8e52f65b554ae772c8
Noiddd <dionang11@gmail.com>
2023-01-18 19:17:07 +0800

    22:39

---
+++ youtube_clone/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  VideoDetail,
+  ChannelDetail,
+  Feed,
+  SearchFeed,
+} from "./components";
+
+const App = () => {
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />}></Route>
+        <Route path="/video/:id" exact element={<VideoDetail />}></Route>
+        <Route path="/channel/:id" exact element={<ChannelDetail />}></Route>
+        <Route
+          path="/search/:searchTerm"
+          exact
+          element={<SearchFeed />}
+        ></Route>
+      </Routes>
+    </Box>
+  </BrowserRouter>;
+};
+
+export default App;
